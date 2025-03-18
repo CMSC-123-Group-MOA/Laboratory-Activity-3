3549cc535146656ceb1c4db360e3356cea072f51
ali1x3 <avilaalex317@gmail.com>
2025-03-18 16:01:15 +0800

    added comments for documentation

--- Main.m
+++ Main.m
@@ -1,8 +1,10 @@
-% Load data from the .data file
+% Load data
 data = dlmread('iris_training.data', ',', 0, 0);  % Read only numeric data
 
-% Extract features (first 4 columns)
-X = data(:, 1:4);
+% Extract features (first 4 columns, excluding the classification on t he 5th column)
+X = data(:, 1:4); %This is the training data
+
+
 
 % Read labels separately (since they are strings)
 fid = fopen('iris_training.data', 'r');  
@@ -12,20 +14,21 @@ fclose(fid);
 y = labels{1};
 
 class_labels = {'Iris-setosa', 'Iris-versicolor', 'Iris-virginica'};
-[~, y] = ismember(y, class_labels);
+[~, y] = ismember(y, class_labels); % this is the outcome for each of the training data
 % Display first few rows to verify
-disp(X);
-disp(y);
+% disp(X);
+% disp(y);
 
 
 input_layer = 4 % 4 features, sepal length&width, petal length&width
 hidden_layer = 10 % arbitrary amount
 num_labels = 3 % 3 classifications, Setosa, Veriscolour, Virginica
 
-Theta1 = randInitializeWeights(input_layer, hidden_layer);
-Theta2 = randInitializeWeights(hidden_layer, num_labels);
+%Random generation of weights
+Theta1 = randInitializeWeights(input_layer, hidden_layer); %Theta1 are the weights between input layer and hidden layer
+Theta2 = randInitializeWeights(hidden_layer, num_labels); %Theta2 are the weights between hidden layer and output layer
 
 nn_params = [Theta1(:); Theta2(:)];
 
-J = nnCostFunction(nn_params, input_layer, hidden_layer, num_labels, X, y, 1);
+J = nnCostFunction(nn_params, input_layer, hidden_layer, num_labels, X, y, 1); %Cost computation of with weights Theta1 and Theta2
 disp(J);

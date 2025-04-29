03d093b58fedc5062686ede8a40b66a5cf346dd9
ali1x3 <avilaalex317@gmail.com>
2025-04-29 17:15:33 +0800

    idk if its working but its something

--- crossover.m
+++
@@ -1,9 +0,0 @@
-function [child1, child2] = crossover(nn_params_1, nn_params_2)
-%CROSSOVER just does a crossover
-
-mask = randi([0,1], length(nn_params_1), 1); % mask vector column full of 0s and 1s
-child1 = mask .* nn_params_1 + (1 - mask) .* nn_params_2; % apply mask to get child1
-mask = randi([0,1], length(nn_params_1), 1); % mask vector column full of 0s and 1s
-child2 = mask .* nn_params_1 + (1 - mask) .* nn_params_2; % apply mask to get child1
-
-end
---
+++ updateVelocity.m
@@ -0,0 +1,26 @@
+function new_velocity = updateVelocity(current_velocity, position, personal_best, global_best, w, c1, c2)
+  % Inputs:
+  % current_velocity, position, personal_best: 1xN cell arrays of 1xD vectors
+  % global_best: 1xD vector (not a cell)
+  % w: inertia weight
+  % c1, c2: cognitive and social coefficients
+
+  N = numel(position); % Number of particles
+
+  new_velocity = cell(1, N); % Initialize output cell array
+
+  for i = 1:N
+    % Generate random vectors of the same size as the particle
+    D = numel(position{i});
+    r1 = rand(1, D);
+    r2 = rand(1, D);
+
+    % Velocity update rule (element-wise operations)
+    inertia    = w  * current_velocity{i};
+    cognitive  = c1 * r1 .* (personal_best{i} - position{i});
+    social     = c2 * r2 .* (global_best - position{i});
+
+    new_velocity{i} = inertia + cognitive + social;
+  end
+end
+

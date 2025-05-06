69cc3af5e056706caaf56f6f51ec1d6eae7e4324
ali1x3 <avilaalex371@gmail.com>
2025-05-06 14:48:58 +0800

    changed rand

--- updateVelocity.m
+++ updateVelocity.m
@@ -10,13 +10,11 @@ function velocity = updateVelocity(current_velocity, position, personal_best, gl
   for i = 1:N
     % Generate random vectors of the same size as the particle
     D = numel(position{i});
-    r1 = rand(1, D);
-    r2 = rand(1, D);
 
     % Velocity update rule (element-wise operations)
     inertia    = w  * current_velocity{i};
-    cognitive  = c1 * r1 * (personal_best{i} - position{i});
-    social     = c2 * r2 * (global_best - position{i});
+    cognitive  = c1 * (rand() * 2 * 0.12 - 0.12) * (personal_best{i} - position{i});
+    social     = c2 * (rand() * 2 * 0.12 - 0.12) * (global_best - position{i});
 
     velocity{i} = inertia + cognitive + social;
   end
